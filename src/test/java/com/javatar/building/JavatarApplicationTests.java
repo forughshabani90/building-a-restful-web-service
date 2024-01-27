b88318899c505c6811f316f7eaf9f637a5387aaf
forughshabani90 <forugh.shabani.90@gmail.com>
2024-01-27 03:35:44 -0800

    first commit

---
+++ src/test/java/com/javatar/building/JavatarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatar.building;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
